ed86821bfcc6515369e0d02d5bff5da127896191
Marcos Apodaca <marcosapodaca@outlook.es>
2023-10-17 17:28:13 +0200

    Cambio en el main, y añadiendo seccion de inscripcion

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const header = document.getElementById('m-header');
+// const header = document.getElementById('m-header');
 
-window.addEventListener('scroll', () => {
-    // Obtén la posición de desplazamiento vertical
-    const scrollY = window.scrollY;
+// window.addEventListener('scroll', () => {
+//     // Obtén la posición de desplazamiento vertical
+//     const scrollY = window.scrollY;
 
-    // Define la altura a la que quieres que cambie el fondo (por ejemplo, 100 píxeles)
-    const scrollThreshold = 50;
+//     // Define la altura a la que quieres que cambie el fondo (por ejemplo, 100 píxeles)
+//     const scrollThreshold = 50;
 
-    if (scrollY > scrollThreshold) {
-        // Cambia el fondo cuando se hace suficiente scroll
-        header.style.backgroundColor = "#000"; // Nuevo color de fondo
-    } else {
-        // Vuelve al fondo original si se desplaza hacia arriba
-        header.style.backgroundColor = 'rgba(0, 0, 0, .5) '; // Color de fondo original
-    }
-});
+//     if (scrollY > scrollThreshold) {
+//         // Cambia el fondo cuando se hace suficiente scroll
+//         header.style.backgroundColor = "#000"; // Nuevo color de fondo
+//     } else {
+//         // Vuelve al fondo original si se desplaza hacia arriba
+//         header.style.backgroundColor = 'rgba(0, 0, 0, .5) '; // Color de fondo original
+//     }
+// });
 
+// console.log(screenY);
