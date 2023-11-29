f5a9f441afa435f0c5aac6dd4b7c031159ff22ee
Marcos Apodaca <marcosapodaca@outlook.es>
2023-11-29 14:05:08 +0100

    Cambios 29/11

--- script.js
+++ script.js
@@ -21,11 +21,57 @@
 const navToggle = document.getElementById("navToggle")
 const menu_nav = document.getElementById("menu_nav")
 
+let parrafo = document.getElementById('texto-largo');
+let parrafo1 = document.getElementById('texto-largo1')
 
+navToggle.addEventListener('click', function() {
+  parrafo.classList.toggle('mostrar');
+});
+
+navToggle.addEventListener('click', function() {
+  parrafo1.classList.toggle('mostrar');
+});
 navToggle.addEventListener("click", () => {
   menu_nav.classList.toggle("hidden")
 });
 
+
+
+const $sectionPin = document.querySelector('#sectionPin');
+const $pinWrapSticky = document.querySelector('.pin-wrap-sticky');
+const $pinWrap = document.querySelector('.pin-wrap');
+
+/* Stretch it out, so that we create room for the horizontal scroll animation */
+$sectionPin.style.height = '500vh';
+$sectionPin.style.overflow = 'visible'; // To make position sticky work …
+
+/* Stick to Top */
+$pinWrapSticky.style.height = '100vh';
+$pinWrapSticky.style.width = '100vw';
+$pinWrapSticky.style.position = 'sticky';
+$pinWrapSticky.style.top = '0';
+$pinWrapSticky.style.overflowX = 'hidden';
+
+/* Stretch out pinwrap */
+$pinWrap.style.height = '100vh';
+$pinWrap.style.width = '250vmax';
+
+// Scroll-Linked Animation
+$pinWrap.animate(
+	{
+		transform: [ ``, `translateX(calc(-100% + 100vw))`],
+		left: '0px',
+	},
+	{
+		timeline: new ViewTimeline({
+			subject: $sectionPin,
+			axis: 'block',
+		}),
+		fill: 'forwards',
+		rangeStart: `contain 0%`,
+		rangeEnd: `contain 100%`,
+	}
+);
 
 // navToggle.addEventListener("click", function() {
 //     if (menu_nav.classList.contains("hidden")) {
